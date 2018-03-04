ef2d4a5a5fea1cfece9ea20f0bc9d97ffc9869f4
oscarrep <oscar.bcn.95@gmail.com>
2018-03-04 22:22:06 +0100

    hello world 2

--- cuadrado/cuadrado/main.cpp
+++ cuadrado/cuadrado/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "../deps/include/SDL.h"
+
+#pragma comment(lib,"../deps/lib/SDL2.lib")
+#pragma comment(lib,"../deps/lib/SDL2main.lib")
+
+using namespace std;
 
 
-int main()
-{
+int main(int argv, char *argc[])
+{	
 	printf("hello world\n");
 	getchar();
+	return 0;
 }
